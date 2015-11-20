2c359724972c9d9c50a626c09fa6d555f4c1f3d5
lordkai5 <qaz111id@gmail.com>
2015-11-19 20:13:20 -0700

    Update go_to_specific_point_on_map.py
    Implemented arguments for GoToPose() function which will be imported to the batterybutton script. x_pose and y_pose
    Extended task duration limit
    renamed node from 'nav_test' to 'nav_target'
    Specialized comments for lunchbot use.

--- go_to_specific_point_on_map.py
+++ go_to_specific_point_on_map.py
@@ -23,37 +23,37 @@ import actionlib
 from actionlib_msgs.msg import *
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point, Quaternion, Twist
 
-class GoToPose():
+class GoToPose(x_pose,y_pose):
     def __init__(self):
-        rospy.init_node('nav_test', anonymous=False)
+        rospy.init_node('nav_target', anonymous=False)
 
 	#what to do if shut down (e.g. ctrl + C or failure)
 	rospy.on_shutdown(self.shutdown)
 
 	
-	#tell the action client that we want to spin a thread by default
+	#tell the action client that we want to spin a thread by default (basically keep it looping)
 	self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
 	rospy.loginfo("wait for the action server to come up")
 	#allow up to 5 seconds for the action server to come up
 	self.move_base.wait_for_server(rospy.Duration(5))
 
-	#we'll send a goal to the robot to tell it to move to a pose that's near the docking station
+	#we'll send a goal to the robot to tell it to move to the specified pose
 	goal = MoveBaseGoal()
 	goal.target_pose.header.frame_id = 'map'
 	goal.target_pose.header.stamp = rospy.Time.now()
-	#customize the following Point() values so they are appropriate for your location
-	goal.target_pose.pose = Pose(Point(2.18, -1.7, 0.000), Quaternion(0.000, 0.000, 0.892, -1.500))
+	#Don't worry about Quaternion. 3rd argument of Pose is simply assumed altitude.
+	goal.target_pose.pose = Pose(Point(x_pose, y_pose, 0.000), Quaternion(0.000, 0.000, 0.892, -1.500))
 
 	#start moving
         self.move_base.send_goal(goal)
 
-	#allow TurtleBot up to 60 seconds to complete task
-	success = self.move_base.wait_for_result(rospy.Duration(60)) 
+	#allow TurtleBot up to 10*6**2 seconds to complete task
+	success = self.move_base.wait_for_result(rospy.Duration(360)) 
 
 
 	if not success:
                 self.move_base.cancel_goal()
-                rospy.loginfo("The base failed to reach the desired pose")
+                rospy.loginfo("ERROR: lunchbot's speed cannot handle this distance within the time parameters")
     	else:
 		# We made it!
 		state = self.move_base.get_state()
